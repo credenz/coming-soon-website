8e36f0746ec11ae37cf204cfab26662c7ad3b4ff
Sakshee <sakshee1120@gmail.com>
2020-12-17 23:27:15 +0530

    Broken image

--- src/HomeComponent/HomeComponent.js
+++ src/HomeComponent/HomeComponent.js
@@ -2,36 +2,23 @@ import React, { useEffect } from "react";
 import Aboutus from "./Aboutus/Aboutus";
 import Sponsors from "./sponsors/sponsors";
 import HomeAnimation from "./HomeAnimation/HomeAnimation";
-import Footer from "../Footer/footer";
 import classes from "./HomeComponent.module.css";
 import Timer from "./Timer";
 
 const Home = () => {
   return (
-    <div>
-      <div className={classes.body}>
+    <div className="container">
+      <div className="d-flex justify-content-center">
+        <img alt="black" src={require('../assests/img/final-logo.png')}></img>
+      </div>
+      <div className="d-flex justify-content-center">
         {/* <h1 style={{color: "ivory"}}>Credenz Home Page</h1> */}
         <HomeAnimation />
-        <div
-          data-aos='fade-up'
-          data-aos-mirror='true'
-          data-aos-offset='200'
-          data-aos-easing='ease-in-sine'
-          data-aos-duration='600'
-          className='d-flex justify-content-center aos-item'>
-          <h1>Coming Soon!</h1>
-          {/*<Timer />*/}
-        
-        <div id='Aboutus'>
-          <Aboutus />
-        </div>
-        <div className={classes.sponsor}>
-          {" "}
-          <Sponsors />
-        </div>
       </div>
+      <div className="d-flex justify-content-center">
+        {/* <h1 style={{color: "ivory"}}>Credenz Home Page</h1> */}
+        <h1>Coming Soon!</h1>
       </div>
-      <Footer />
     </div>
   );
 };
