4521b1586cea192a54d2daea39be8861d8032665
Sakshee <sakshee1120@gmail.com>
2020-12-19 08:20:39 +0530

    commit

--- src/HomeComponent/HomeComponent.js
+++ src/HomeComponent/HomeComponent.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
-import Aboutus from "./Aboutus/Aboutus";
-import Sponsors from "./sponsors/sponsors";
 import HomeAnimation from "./HomeAnimation/HomeAnimation";
-import classes from "./HomeComponent.module.css";
 import Timer from "./Timer";
 
 const Home = () => {
   return (
     <div className="container">
       <div className="d-flex justify-content-center">
-        <img alt="black" src={require('../assests/img/final-logo.png')}></img>
+        <img alt="black" src={require('./final-logo.png')}></img>
       </div>
       <div className="d-flex justify-content-center">
         {/* <h1 style={{color: "ivory"}}>Credenz Home Page</h1> */}
